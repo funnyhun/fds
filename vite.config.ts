8deec8e5c202d8b664e1fa91cea76ba8f77db89c
funnyhun <funnyhun0222@gmail.com>
2026-03-05 18:28:25 +0900

    feat: 배포 환경에서 404 페이지 및 경로 복구 모듈 추가
    - github-page는 정적 호스팅 서비스에서 404(not found) 및 redirect에 대한 별도 유틸리티 지원 x
     - 기본 웹 구조에서 404.html 추가(서비스에 not found page가 이미 존재)
     - 위 404.html은 새로고침에 의한 경로 유실 혹은 invalid-path에 대해 index.html로 redirect 하는 역할
     - index.html의 redirect module은 404.html에서 추출한 ?p=[:path] 파라미터를 사용해 경로를 유지

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/fds/" : "/",
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/fds/" : "/",
+    plugins: [react()],
+  };
 });
